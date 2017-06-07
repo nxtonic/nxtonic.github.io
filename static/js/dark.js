b6e3c818f2dab822f143b401338de877ff499533
Angus McMillan <nxtonic@gmail.com>
2017-06-07 18:08:06 +1000

    improved dark-mode timing and execution

--- static/js/dark.js
+++ static/js/dark.js
@@ -1,12 +1,14 @@
-var link = document.createElement('link');
-link.setAttribute('rel', 'stylesheet');
-link.type = 'text/css';
-link.href = '/static/css/dark.css';
-var date = new Date;
-var hours = date.getHours();
+(function(){
+  var link = document.createElement('link');
+  link.setAttribute('rel', 'stylesheet');
+  link.type = 'text/css';
+  link.href = '/static/css/dark.css';
+  var date = new Date;
+  var hours = date.getHours();
 
-if (hours < 6 || hours > 18) {
-  setTimeout(function () {
-    document.head.appendChild(link);
-  }, 500);
-};
+  if (hours < 6 || hours >= 18) {
+    setTimeout(function () {
+      document.head.appendChild(link);
+    }, 500);
+  }
+})();
